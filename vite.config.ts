0bc147acc93ae24c79245b238f4f67ccc4cf7ad0
Tima Gixe <timagixe@protonmail.com>
2025-06-15 12:54:46 +0300

    chore: update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react({
+            babel: {
+                babelrc: true,
+            },
+        }),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
