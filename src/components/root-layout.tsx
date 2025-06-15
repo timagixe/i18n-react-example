8938d08067bc68bf447ed3be5d8b911e151e3b8b
Tima Gixe <timagixe@protonmail.com>
2025-06-15 20:06:00 +0300

    feat: add footer

--- src/components/root-layout.tsx
+++ src/components/root-layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Navigation } from "./navigation";
+import { Footer } from "./footer";
 
 export function RootLayout() {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <Navigation />
-            <main>
+            <main className="flex-grow">
                 <Outlet />
             </main>
-        </>
+            <Footer />
+        </div>
     );
 }
