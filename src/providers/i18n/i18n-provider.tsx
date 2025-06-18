e9743344d9c9d3569cc3ee026878d08f7351247c
Tima Gixe <timagixe@protonmail.com>
2025-06-18 10:07:53 +0300

    feat: setup basic translation lazy loading

--- src/providers/i18n/i18n-provider.tsx
+++ src/providers/i18n/i18n-provider.tsx
@@ -1,24 +1,45 @@
 import type { PropsWithChildren } from "react";
 import { IntlProvider } from "react-intl";
 import { useLocaleContext } from "../locale";
+import { useEffect, useState } from "react";
 import enMessages from "./locales/en.json";
-import esMessages from "./locales/es.json";
-import arMessages from "./locales/ar.json";
 
-const _messages: Record<string, Record<string, string>> = {
-    en: enMessages,
-    es: esMessages,
-    ar: arMessages,
-};
+function useLocaleMessages(locale: string) {
+    const [messages, setMessages] = useState<Record<string, string>>(enMessages);
+
+    useEffect(() => {
+        const loadMessages = async () => {
+            try {
+                let loadedMessages;
+                switch (locale) {
+                    case "es":
+                        loadedMessages = (await import("./locales/es.json")).default;
+                        break;
+                    case "ar":
+                        loadedMessages = (await import("./locales/ar.json")).default;
+                        break;
+                    case "en":
+                    default:
+                        loadedMessages = enMessages;
+                        break;
+                }
+                setMessages(loadedMessages);
+            } catch (error) {
+                console.error("Error loading messages", error);
+            }
+        };
+        loadMessages();
+    }, [locale]);
+
+    return { messages };
+}
 
 export function I18NProvider({ children }: PropsWithChildren) {
     const localeContext = useLocaleContext();
-
-    const locale = localeContext.locale in _messages ? localeContext.locale : "en";
-    const messages = _messages[locale];
+    const { messages } = useLocaleMessages(localeContext.locale);
 
     return (
-        <IntlProvider locale={locale} messages={messages}>
+        <IntlProvider locale={localeContext.locale} messages={messages}>
             {children}
         </IntlProvider>
     );
