0db5d0aa63ea52feeb02669d3b844427ffbd6f6e
Tima Gixe <timagixe@protonmail.com>
2025-06-25 10:18:06 +0300

    refactor: cleanup footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,11 @@
 import { Card, CardHeader } from "@/components/ui/card";
-import { FormattedMessage } from "react-intl";
-import { messages } from "./footer.messages";
 
 export function Footer() {
     return (
         <Card className="m-4 mt-auto">
             <CardHeader>
                 <div className="text-center text-muted-foreground">
-                    <FormattedMessage
-                        {...messages.copyright}
-                        values={{
-                            year: new Date(),
-                        }}
-                    />
+                    © {new Date().getFullYear()} Your Company Name. All rights reserved.
                 </div>
             </CardHeader>
         </Card>
