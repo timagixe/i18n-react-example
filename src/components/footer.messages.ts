477f472b22138ab98217ae08e8cce457882aa8c9
Tima Gixe <timagixe@protonmail.com>
2025-06-25 10:18:52 +0300

    refactor: delete messages

--- src/components/footer.messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from "react-intl";
-
-export const messages = defineMessages({
-    copyright: {
-        description: "The copyright message in the footer",
-        defaultMessage: "© {year, date, ::yyyy} Your Company Name. All rights reserved.",
-    },
-});
